3cc5f2d48dcdf8122ae1da215f52774bd7d2788e
mw2020r-creator <mw2020r@gmail.com>
2025-11-03 22:54:46 +0100

    update

--- 2. Sortowanie przez wybieranie/sortowanie przez wybieranie.py
+++ 2. Sortowanie przez wybieranie/sortowanie przez wybieranie.py
@@ -1,4 +1,4 @@
-# sortowanie przez wybieranie (ang. selection sort)
+# selection sort
 
 import os
 import string
@@ -7,36 +7,34 @@ import gc
 import matplotlib.pyplot as plt
 
 
-def selection_sort(slowa):
-    slowa = slowa.copy()
-    for i in range(0, len(slowa)):
+def selection_sort(words):
+    words = words.copy()
+    for i in range(0, len(words)):
         min_idx = i
-        for j in range(i +1,len(slowa)):
-            if slowa[j] < slowa[min_idx]:
+        for j in range(i +1,len(words)):
+            if words[j] < words[min_idx]:
                 min_idx = j
-        slowa[i], slowa[min_idx] = slowa[min_idx], slowa[i]
-    return slowa
+        words[i], words[min_idx] = words[min_idx], words[i]
+    return words
 
 # wczytanie pliku
-sciezka_skryptu = os.path.dirname(__file__)
-plik = os.path.join(sciezka_skryptu, "pan-tadeusz.txt")
 
 with open('pan-tadeusz.txt', 'r', encoding='utf-8') as f:
-    zawartosc_pliku = f.read()
-    slowa = zawartosc_pliku.lower().split()
+    file_content = f.read()
+    words = file_content.lower().split()
 
 #oczyszczanie tekstu przez pominiecie w sortowaniu podstawowej interpunkcji + dodatkowej
-def oczysc_slowo(s):
+def clean_word(s):
     return s.strip(string.punctuation + "«»…")
 
-czyste_slowa = [oczysc_slowo(s) for s in slowa if oczysc_slowo(s)]
+clean_words = [clean_word(s) for s in words if clean_word(s)]
 
 # usuwanie duplikatów za pomocą set
-unikalne_slowa = list(set(czyste_slowa))
+unique_words = list(set(clean_words))
 
 # test na pierwszych 100 słowach
-test_sortowania = selection_sort(slowa[:1000])  
-if test_sortowania == sorted(slowa[:1000], key=str.lower):
+sort_test = selection_sort(words[:1000])  
+if sort_test == sorted(words[:1000], key=str.lower):
     print("selection_sort: poprawnie sortuje")
 else:
     print("selection_sort: BŁĄD SORTOWANIA!")
@@ -44,25 +42,25 @@ else:
 
 # czas sortowania i odśmiecanie
 
-rozmiar_listy = [1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000]
-czas_sortowania = []
+list_size = [1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000]
+sort_time = []
 
-for n in rozmiar_listy:
-    lista_testowa = slowa[:n]
+for n in list_size:
+    test_list = words[:n]
     gc_old = gc.isenabled()
     gc.disable() 
 
     start = time.process_time() 
-    selection_sort(lista_testowa)
+    selection_sort(test_list)
     stop = time.process_time()
     if gc_old: gc.enable() 
-    czas_sortowania.append(stop - start)
+    sort_time.append(stop - start)
     print('Czas wykonania w sekundach:', stop - start)
 
 # wykres
-plt.plot(rozmiar_listy, czas_sortowania, marker='o', linestyle='-', color='pink')
+plt.plot(list_size, sort_time, marker='o', linestyle='-', color='pink')
 plt.title("Czas sortowania")
 plt.xlabel("Liczba elementów")
 plt.ylabel("Czas [s]")
 plt.grid(True)
-plt.savefig("wykres_sort_przez_wybieranie.png") 
+plt.savefig("selection sort.png") 
